ff5175a4ad16025c403c10c4e4a431ddc0329267
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-11 18:18:27 -0500

    feat: :lipstick: Added a logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,19 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import logo from '../public/logo.png'
 import styles from '../styles/Navbar.module.scss'
 
 function Navbar() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <div className={styles.brand}>
-          <Link href='/'>Josué Martell</Link>
+        <div>
+          <Link href='/'>
+            <a className={styles.logo}>
+              <Image src={ logo } width='40' height='40' alt='Logo' />
+              <p className={styles.name}>Josué Martell</p>
+            </a>
+          </Link>
         </div>
         <div className={styles.links}>
           <Link href='/'>Home</Link>
