019f4a36070845d925787d470a852c2913188cea
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-11 19:08:50 -0500

    feat: :sparkles: Added head metadata

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Head>
+        <title>Portfolio | Josué Martell</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <h1>Hello World!</h1>
+    </>
   )
 }
