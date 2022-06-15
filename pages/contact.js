01306dfea8c0f83f8183145e2df6a132e44734d8
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-14 21:37:00 -0500

    feat: :sparkles: Added social links to contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Contact.module.scss'
 
 function Contact() {
@@ -9,7 +11,6 @@ function Contact() {
       if (!field.name) return
       formData[field.name] = field.value
     })
-    console.log(formData)
 
     fetch('/api/mail', {
       method: 'POST',
@@ -27,13 +28,70 @@ function Contact() {
       <div className={styles.main}>
         <h1>Get in contact!</h1>
         <div className={styles.contact}>
+          <div className={styles.socials}>
+            <Link href={'https://twitter.com/josushu0'}>
+              <a target='_blank'>
+                <Image
+                  src={
+                    'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg'
+                  }
+                  alt='Twitter'
+                  width={40}
+                  height={40}
+                />
+                <p>Twitter</p>
+              </a>
+            </Link>
+            <Link href={'https://linkedin.com/in/josuemartell'}>
+              <a target='_blank'>
+                <Image
+                  src={
+                    'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg'
+                  }
+                  alt='Linkedin'
+                  width={40}
+                  height={40}
+                />
+                <p>Linkedin</p>
+              </a>
+            </Link>
+            <Link href={'https://github.com/josushu0'}>
+              <a target='_blank'>
+                <Image
+                  src={
+                    'https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg'
+                  }
+                  alt='Github'
+                  width={40}
+                  height={40}
+                />
+                <p>Github</p>
+              </a>
+            </Link>
+          </div>
           <form className={styles.form} method='post' onSubmit={handleOnSubmit}>
             <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='name' />
+            <input
+              type='text'
+              name='name'
+              id='name'
+              placeholder='Your name...'
+            />
             <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='email' />
+            <input
+              type='email'
+              name='email'
+              id='email'
+              placeholder='Your email...'
+            />
             <label htmlFor='message'>Message</label>
-            <textarea name='message' id='message' cols='30' rows='5'></textarea>
+            <textarea
+              name='message'
+              id='message'
+              cols='30'
+              rows='5'
+              placeholder='Your message...'
+            ></textarea>
             <button>Enviar</button>
           </form>
         </div>
