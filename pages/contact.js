261c94a1fc81312bbc80af97520bc81549f6721f
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-14 20:01:41 -0500

    feat: :sparkles: Created basic form

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,16 @@ import Head from 'next/head'
 import styles from '../styles/Contact.module.scss'
 
 function Contact() {
+  const handleOnSubmit = async (e) => {
+    e.preventDefault()
+    const formData = {}
+    Array.from(e.currentTarget).forEach((field) => {
+      if (!field.name) return
+      formData[field.name] = field.value
+    })
+    console.log(formData)
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +20,18 @@ function Contact() {
         <meta name='description' content='Contact Me' />
       </Head>
       <div className={styles.main}>
-        <h1>Coming soon!</h1>
+        <h1>Get in contact!</h1>
+        <div className={styles.contact}>
+          <form className={styles.form} method='post' onSubmit={handleOnSubmit}>
+            <label htmlFor='name'>Name</label>
+            <input type='text' name='name' id='name' />
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='email' />
+            <label htmlFor='message'>Message</label>
+            <textarea name='message' id='message' cols='30' rows='5'></textarea>
+            <button>Enviar</button>
+          </form>
+        </div>
       </div>
     </>
   )
