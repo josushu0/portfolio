9d22546f3992d6e526fd50f0bd2691dc6515d5a9
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-21 18:36:57 -0500

    feat: :sparkles: Added hambrguer menu icon to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,6 +16,29 @@ function Navbar() {
           <Link href='/projects'>Projects</Link>
           <Link href='/contact'>Contact</Link>
         </div>
+        <div className={styles.drawer}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={2}
+            width={28}
+            height={28}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+          {/* <div>
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/projects'>Projects</Link>
+            <Link href='/contact'>Contact</Link>
+          </div> */}
+        </div>
       </nav>
     </header>
   )
