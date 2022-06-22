bbf8cf03f16de044b10b039100487e721900c88b
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-06-22 00:05:41 -0500

    refactor: :recycle: Log errors in mail api

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,23 +2,27 @@ const sendgrid = require('@sendgrid/mail')
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 let mail = (req, res) => {
-  const body = JSON.parse(req.body)
-  const message = `
+  try {
+    const body = JSON.parse(req.body)
+    const message = `
     Name: ${body.name}\r\n
     Email: ${body.email}\r\n
     Message: ${body.message}\r\n
   `
 
-  const data = {
-    to: 'josue.martell17@gmail.com',
-    from: 'josue.martell17@gmail.com',
-    subject: 'Profolio mail',
-    text: message,
-    html: message.replace(/\r\n/g, '<br/>'),
-  }
+    const data = {
+      to: 'josue.martell17@gmail.com',
+      from: 'josue.martell17@gmail.com',
+      subject: 'Profolio mail',
+      text: message,
+      html: message.replace(/\r\n/g, '<br/>'),
+    }
 
-  sendgrid.send(data)
-  res.status(200).json({ status: 'OK' })
+    sendgrid.send(data)
+    res.status(200).json({ status: 'OK' })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default mail
