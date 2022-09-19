4554ce8b961ef0fe46fa8d20895b065c31a7bd30
Josué Martell <josue.martell17@gmail.com>
2022-09-18 22:34:36 -0500

    feat: added vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://josushu0.github.io',
-	base: '/portfolio',
+  site: 'https://josushu0.github.io',
+  base: '/portfolio',
+  integrations: [vue()]
 });
